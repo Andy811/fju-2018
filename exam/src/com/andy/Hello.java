444f456a99e14e8a79696186fa70c422fbd7ffe5
Andy <user@es302n35>
2018-10-02 11:01:40 +0800

    practice

--- exam/src/com/andy/Hello.java
+++ exam/src/com/andy/Hello.java
@@ -3,7 +3,16 @@ package com.andy;
 public class Hello {
 
 	public static void main(String[] args) {
+		String name = "¤ý¾å©ú";
+		int age = 1000;
+		float weight = 99.9f;
+		float height = 199.9f;
+
 		System.out.println("Hello");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(weight);
+		System.out.println(height);
 	}
 
 }
