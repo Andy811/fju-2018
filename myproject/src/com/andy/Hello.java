4c4fffcd80d0c000aaa5f4d67188a9a0cea41afe
Andy <user@es302n35>
2018-10-02 10:36:34 +0800

    variables

--- myproject/src/com/andy/Hello.java
+++ myproject/src/com/andy/Hello.java
@@ -3,7 +3,15 @@ package com.andy;
 public class Hello {
 
 	public static void main(String[] args) {
-System.out.println("Hello World");
+		int age = 19;
+		String name = "Andy";
+		float weight = 55.5f;
+		float height = 171.5f;
+		System.out.println("Hello World");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(weight);
+		System.out.println(height);
 	}
 
 }
